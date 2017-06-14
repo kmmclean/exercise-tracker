9bd13e33101a1db1bf99a55060106487af3b2d10
Kyle McLean <kmcle7zk@gmail.com>
2017-06-14 18:33:48 -0400

    Initial commit.

---
+++ src/test/java/com/example/exercisetracker/ExerciseTrackerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.exercisetracker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExerciseTrackerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
